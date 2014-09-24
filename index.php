70ce4baab9c34d8fac24e27341b85c6f0c37af78
ityler <tylernormile@gmail.com>
2014-09-23 22:25:47 -0400

    Added index.php

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='http://fonts.googleapis.com/css?family=Special+Elite' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 50px;
+                padding-bottom: 20px;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <div class="mainTop">
+    <div class="ov">
+    <div class="topWrapper">
+    <div class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">PDF-Tools</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <div class="navbar-right">
+          <ul class="social-links">
+            <a href="#"><i class="fa fa-twitter fa-2x"></i></a><!-- Twitter Link -->
+            <a href="#"><i class="fa fa-facebook fa-2x"></i></a><!-- Facebook Link -->
+            </ul>
+          </div>
+        </div><!--/.navbar-collapse -->
+      </div>
+    </div>
+    <div class="pageJumbo">
+      <div class="container" style="text-align:center">
+        <h2>Combine PDF's</h2>
+        <p>Drag and drop PDF files anywhere on this page to start!</p>
+        <div class="hldr">
+            <button id="cmb" type="submit" class="btn btn-primary cmb">
+                <i class="fa fa-file-pdf-o fa-2x"> Create</i> 
+            </button>
+            <div id="status" class="status">
+            <div id="loading"></div>
+      </div>
+        </div>
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    <div class="container files">
+        <div id="filestats" class="filestats"></div>
+    
+    </div>
+    
+    <div class="container">
+      <footer>
+       <!-- <p>&copy; PDF-Tools 2014</p>-->
+      </footer>
+
+    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script src="js/vendor/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+            e.src='//www.google-analytics.com/analytics.js';
+            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+            ga('create','UA-XXXXX-X');ga('send','pageview');
+        </script>
+    </body>
+</html>
